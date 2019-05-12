95d8a60e7a45405dfbd4fbce21b281e341dad35c
OmBram <50556640+OmBram@users.noreply.github.com>
2019-05-13 04:52:52 +0700

    Update and rename alice.sh to TanyaOmBram.sh

--- TanyaOmBram.sh
+++ TanyaOmBram.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
 #///////////////////////////////////////////////////////////////////////////////
 #///////////////////////////////////////////////////////////////////////////////
-#////                       _            _  __                              ////
-#////                      | |          (_)/ _|                             ////
-#////                   ___| |_   _  ___ _| |_ ___ _ __                     ////
-#////                  |_  / | | | |/ __| |  _/ _ \ '__|                    ////
-#////                   / /| | |_| | (__| | ||  __/ |                       ////
-#////                  /___|_|\__,_|\___|_|_| \___|_|                       ////
+#////                     ___              ____                             ////
+#////                    / _ \ _ __ ___   | __ ) _ __ __ _ _ __ ___         ////
+#////                   | | | | '_ ` _ \  |  _ \| '__/ _` | '_ ` _ \        ////
+#////                   | |_| | | | | | | | |_) | | | (_| | | | | | |       ////
+#////                    \___/|_| |_| |_| |____/|_|  \__,_|_| |_| |_|       ////
 #////                                                                       ////
 #///////////////////////////////////////////////////////////////////////////////
 #///////////////////////////////////////////////////////////////////////////////
 N='\033[0;m'
 HIJAU='\033[5;49;92m'
 MERAH='\033[0;31m'
-echo Selamat datang kak, Siapa nick kaka? #tulisan keluar
+echo Selamat datang kak, Siapa nick kaka? Pasti Jomblo Ya#tulisan keluar
 
 read nick #membaca yang ditulis
 echo "================================================="
@@ -21,20 +20,20 @@ echo "== ╦ ╦┌─┐┌─┐┬┌─┌─┐┬─┐  ┬┌┐┌┌�
 echo "== ╠═╣├─┤│  ├┴┐├┤ ├┬┘  ││││ │││ ││││├┤ └─┐│├─┤ =="
 echo "== ╩ ╩┴ ┴└─┘┴ ┴└─┘┴└─  ┴┘└┘─┴┘└─┘┘└┘└─┘└─┘┴┴ ┴ =="
 echo "================================================="
-echo Selamat datang $nick ":)" nama ku ALICE
+echo Selamat datang $nick ":)" nama ku Om Bram
 echo $MERAH"Untuk keluar ketik x lalu enter"$N
 echo
 echo "Silahkan tanyakan sesuatu"
 echo Contoh :
-echo cara pakai termux insta
+echo cara coli eh maaf (cara pakai termux insta)
 read ASK # masukin pertanyaan
 FIX="$( echo "$ASK" | sed 's/[[:space:]]/_/g')"
 while true; do
 if [ $FIX = "x" ]; then
 exit 1
 else
-alice=`curl -s http://zlucifer.hol.es/Project_Alice/index.php?input=$FIX`
-echo $HIJAU"$alice"$N
+Om Bram=`curl -s http://zlucifer.hol.es/Project_Alice/index.php?input=$FIX`
+echo $HIJAU"$Om Bram"$N
 read ASK # masukin pertanyaan
 FIX="$( echo "$ASK" | sed 's/[[:space:]]/_/g')"
 fi
